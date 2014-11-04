76982d33226c881871c3f328892d881ba7d3e3ad
Didrik Sæther <didrik.sather@gmail.com>
2014-11-04 19:13:16 +0100

    5. times the charm, maby not.. This does not twerk

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
+var square = null;
+
 function drawSquare(){
 
 	var c = document.getElementById("Canvas");
 	var ctx = c.getContext("2d");
 	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(makeSquare.x, makeSquare.y ,10,10);
+	ctx.fillRect(0,0,15,15);
+}
+
+function move() {
+	if square === null{
+		return;
+	}
+	var c = document.getElementById("Canvas");
+	var ctx = c.getContext("2d");
+	ctx.clearRect(square.x, square.y, square.h, square.w);
+	square.x=+2;
+	square.y=+2;
+	drawSquare();
+
 }
 
 function makeSquare() {
-	x = 0;
-	y = 0;
-	h = 15;
-	w = 15;
+	square = {
+	x = 0,
+	y = 0,
+	h = 15,
+	w = 15
 }
-
-document.getElementById("Canvas").addEventListener('click', drawSquare);
-setInterval(function(){makeSquare()}, 1000);
+}
+setInterval(function(){move()}, 1000);
+document.getElementById("Canvas").addEventListener('click', makeSquare);
