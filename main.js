38542cea3c0de686d5c432881c28d08b40ff9914
Didrik Sæther <didrik.sather@gmail.com>
2014-11-04 18:54:56 +0100

    4. times the charm

--- main.js
+++ main.js
@@ -2,8 +2,16 @@ function drawSquare(){
 
 	var c = document.getElementById("Canvas");
 	var ctx = c.getContext("2d");
-	ctx.fillStyle = "#000000";
-	ctx.fillRect(0,0,400,400);
-
+	ctx.fillStyle = "#FF0000";
+	ctx.fillRect(makeSquare.x, makeSquare.y ,10,10);
 }
+
+function makeSquare() {
+	x = 0;
+	y = 0;
+	h = 15;
+	w = 15;
+}
+
 document.getElementById("Canvas").addEventListener('click', drawSquare);
+setInterval(function(){makeSquare()}, 1000);
