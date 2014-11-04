f4545d78b85fbc0deea85308e498d74645198708
Didrik Sæther <didrik.sather@gmail.com>
2014-11-04 19:22:38 +0100

    6. time, Things are moving.. Oh yeah

--- main.js
+++ main.js
@@ -5,29 +5,28 @@ function drawSquare(){
 	var c = document.getElementById("Canvas");
 	var ctx = c.getContext("2d");
 	ctx.fillStyle = "#FF0000";
-	ctx.fillRect(0,0,15,15);
+	ctx.fillRect(square.x,square.y,square.h,square.w);
 }
 
 function move() {
-	if square === null{
+	if (square === null){
 		return;
 	}
 	var c = document.getElementById("Canvas");
 	var ctx = c.getContext("2d");
 	ctx.clearRect(square.x, square.y, square.h, square.w);
-	square.x=+2;
-	square.y=+2;
+	square.x+=2;
+	square.y+=2;
 	drawSquare();
-
 }
 
 function makeSquare() {
 	square = {
-	x = 0,
-	y = 0,
-	h = 15,
-	w = 15
+	x: 0,
+	y: 0,
+	h: 15,
+	w: 15
 }
 }
-setInterval(function(){move()}, 1000);
+setInterval(move, 1000/60);
 document.getElementById("Canvas").addEventListener('click', makeSquare);
